d350cf8707070fc87385f4faa4a3cfd4b8b891ad
DESKTOP-N2E7I99\pablo <pablomtecnolo>
2025-04-26 15:05:01 +0200

    cambio environment.prod para apuntar front a servidor ec2

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,9 @@
+// export const environment = {
+//   production: true,
+//   apiUrl: 'http://10.0.2.2:8080'
+// };
+// console.log("🔧 Usando environment.prod.ts");
 export const environment = {
   production: true,
-  apiUrl: 'http://10.0.2.2:8080'
+  apiUrl: 'http://34.205.144.1:8080'  // O como lo tengas estructurado
 };
-console.log("🔧 Usando environment.prod.ts");
